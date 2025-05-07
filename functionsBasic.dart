fdce1529a4292a8409ed67025927419e42536965
Greeceuniquecode <notgreece2@gmail.com>
2025-05-07 12:14:22 +0545

    basic Functions

---
+++ functionsBasic.dart
@@ -0,0 +1,17 @@
+//functions in Dart by GreeceUniquecode
+int add(int a, int b) {
+  // Creating function
+  int result = a + b;
+
+  // returning value result
+  return result;
+}
+
+void main() {
+  // Calling the function
+  var output = add(10, 20);
+
+  // Printing output
+  print(output);
+}
+//follow for more freshly brewed content from Greeceuniquecode
